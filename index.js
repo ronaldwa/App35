8320f5132bb772a87f0d93de6a6968c825285d48
ronaldwa <ronaldwarmels@gmail.com>
2016-01-05 14:48:27 +0100

    website example

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var urldb = 'postgres://fmnphruqkxxdev:54CeYQTVb_C2JGzeARymlGM_Ty@ec2-54-204-39-67.compute-1.amazonaws.com:5432/dd5farsl3q18a0?ssl=true';
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,7 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/', function(request, response) {
+response.render('pages/index');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log('Ronald is die baas');
+  console.log('je moeder is zo dik dat ze in haar eentje al groepskorting krijgt');
 });
