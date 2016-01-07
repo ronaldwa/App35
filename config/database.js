dfb220b814ff13e9c98f3c3e28cbf7fe342cae7b
ronaldwa <ronaldwarmels@gmail.com>
2016-01-07 20:16:11 +0100

    inlog system

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://ronaldwarmels:wachtwoord@ds035985.mongolab.com:35985/songs' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
