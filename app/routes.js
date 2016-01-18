fccff919cade99ee19f9c9b18ec071db86ece7b9
JurjenFaber <jurjenfaber94@hotmail.com>
2016-01-18 21:48:15 +0100

    Last bugfix?

--- app/routes.js
+++ app/routes.js
@@ -63,7 +63,7 @@ module.exports = function(app, passport) {
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-       successRedirect : '/login/succes', // redirect to the secure profile section
+       successRedirect : '/profile', // redirect to the secure profile section
        failureRedirect : '/signup', // redirect back to the signup page if there is an error
        failureFlash : true // allow flash messages
    }));
@@ -74,6 +74,10 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('pages/profile.ejs', {
+            user : req.user, // get the user out of session and pass to template
+            lastFiveRatings: 0
+        });
         profile.load(req.user, req, res);
     });
 
